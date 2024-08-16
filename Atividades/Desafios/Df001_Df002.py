9f06b34b93154b4d9e32590ac99060a069a83a61
Kelvyn Gustavo Humanes <gustavohumanes2007@gmail.com>
2024-08-15 22:12:21 -0300

    Inicianod com o python

---
+++ Atividades/Desafios/Df001_Df002.py
@@ -0,0 +1,6 @@
+Nome = input("Qual o seu nome?")
+Dia = input("Qual o dia do seu aniversário?")
+Mes = input("Qual o mês do seu aniversário?")
+Ano = input("Qual a sua data de nascimento?")
+
+print(f"Olá, {Nome} Você nasceu em {Dia} do {Mes} do ano de {Ano}!")
