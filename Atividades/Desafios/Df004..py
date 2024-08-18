f50b61b72f4ea22c095d24f0fafcc0a63f4be095
Kelvyn Gustavo Humanes <gustavohumanes2007@gmail.com>
2024-08-17 21:47:34 -0300

    Testando os métodos padrões "type()" e "isalgumacoisa()"

---
+++ Atividades/Desafios/Df004..py
@@ -0,0 +1,7 @@
+Tecla_Pressionado = int(input("Digite alguma coisa>"))
+
+
+print(f"O valor primitivo desse elemento é{type(Tecla_Pressionado)}")
+print(f"O valor só tem espaços? {Tecla_Pressionado.isspace()}")
+
+
