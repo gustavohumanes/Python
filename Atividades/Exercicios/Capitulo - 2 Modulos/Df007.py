c81e05619ae56d5be2c045ee84fe081638b2e6a8
Kelvyn Gustavo Humanes <reactthefandoms34222682@gmail.com>
2024-08-24 13:09:33 -0300

    Verificando o random Shuffle

--- Atividades/Exercicios/Capitulo - 2 Modulos/Df007.py
+++ Atividades/Exercicios/Capitulo - 2 Modulos/Df007.py
@@ -1,10 +1,11 @@
-from random import shuffle
-n1 = str(input("Digite um nome: "))
-n2 = str(input("Digite outro nome: "))
-n3 = str(input("Mais um, bora fi: "))
-n4 = str(input("Esse é o último: "))
+import random
+
+n1 = str(input("Digite o nome do aluno 1: "))
+n2 = str(input("Digite o nome do aluno 2: "))
+n3 = str(input("Digite o nome do aluno 3: "))
+n4 = str(input("Digite o nome do aluno 4: "))
 
 Nomes = [n1, n2, n3, n4]
-shuffle(Nomes)
+random.shuffle(Nomes)
 
-print(f"A ordem sorteada foi: {Nomes}")
+print(f"A ordem será: {Nomes}")
