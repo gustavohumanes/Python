66fc839118ae517b27e93db2c7756eb69f7c4afa
Kelvyn Gustavo Humanes <reactthefandoms34222682@gmail.com>
2024-09-08 13:58:13 -0300

    Aprendendo a tocar musica

---
+++ Atividades/Exercicios/Capitulo - 2 Modulos/Df008.py
@@ -0,0 +1,8 @@
+import pygame
+pygame.init()
+
+pygame.mixer.music.load('D:\Gustavo\Visual Studio CODE\ProjetoCursoEmvideo\Python\Atividades\Exercicios\Capitulo - 2 Modulos\Midia/musica.mp3')
+pygame.mixer.music.play()
+
+input()
+pygame.event.wait()
